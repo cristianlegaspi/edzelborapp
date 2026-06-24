48d396bc3f9a62ed7f87ef1a824cd26596ced36d
unknown <pjmesteves@tesda.gov.ph>
2026-06-24 10:06:43 +0800

    FIXED THE filter

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('EDZEBOR FUEL TRADING WEB APP')
+            ->brandName('EDZELVOR FUEL TRADING WEB APP')
             ->colors([
                 'primary' => Color::Amber,
             ])
